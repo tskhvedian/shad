0eedceda3618188c8b8681fd594e473864af9d22
tskhvedian <t.tskhvediani@gmail.com>
2024-03-07 15:41:26 +0400

    email input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./components/ui/form";
+import { Input } from "./components/ui/input";
+
+const formSchema = z.object({
+  emailAddress: z.string().email(),
+});
 
 const App = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      emailAddress: "",
+    },
+  });
+
+  const handleSubmit = () => {};
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>HELLO</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-4">
+     
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)}>
+          <FormField
+            control={form.control}
+            name="emailAddress"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Email Address</FormLabel>
+                  <FormControl>
+                    <Input />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+        </form>
+      </Form>
     </main>
   );
 };
